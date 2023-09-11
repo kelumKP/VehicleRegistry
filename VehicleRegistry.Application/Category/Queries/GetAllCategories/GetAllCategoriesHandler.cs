521975fcc0391c03c553a85e917c61e943fb6c66
kelumkp <kelumgit@outlook.com>
2023-09-11 22:51:36 +1200

    GetAllCategoriesQuery registered

---
+++ VehicleRegistry.Application/Category/Queries/GetAllCategories/GetAllCategoriesHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleRegistry.DAL;
+
+namespace VehicleRegistry.Application.Category.Queries.GetAllCategories
+{
+    public class GetAllCategoriesHandler : IRequestHandler<GetAllCategoriesQuery, List<Core.Models.Category>>
+    {
+        private readonly DataContext _ctx;
+        public GetAllCategoriesHandler(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<List<Core.Models.Category>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _ctx.Categories.ToListAsync();
+        }
+    }
+}
---
+++ VehicleRegistry.Application/Category/Queries/GetAllCategories/GetAllCategoriesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRegistry.Application.Category.Queries.GetAllCategories
+{
+    public class GetAllCategoriesQuery : IRequest<List<Core.Models.Category>>
+    {
+    }
+}
